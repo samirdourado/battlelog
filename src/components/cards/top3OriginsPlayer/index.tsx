af46000c10820c157a7bd7a056322491527467e1
samirdourado <samir3500@gmail.com>
2024-04-28 23:19:44 -0300

    Feat card holder and card

--- src/components/cards/top3OriginsPlayer/index.tsx
+++ src/components/cards/top3OriginsPlayer/index.tsx
@@ -1,8 +1,90 @@
+import Image from "next/image"
+import styles from "./styles.module.scss"
+
 const Top3OriginsPlayerCard = () => {
 
-    return (
-        <li>
-            <p>Card aqui</p>            
+    return (        
+        <li className={styles.card__container}>
+
+            <section className={styles.card__top}>
+                <section>
+                    <h3>1</h3>
+                    <p>Player Name</p>
+                </section>
+                <section>
+                    <p>2231</p>
+                    <p>Clã Name</p>
+                </section>
+            </section>
+
+            <hr className={styles.card__line}></hr>
+                
+            <ul className={styles.card__bottom}>
+                <li>
+                    <Image
+                        src={"/axie-provisory.png"}
+                        alt={"Axie Name"}
+                        width={50}
+                        height={40}
+                    />
+                    <section>
+                        <Image
+                            src={"/rune-provisory.png"}
+                            alt={"Axie Parts name"}
+                            width={18}
+                            height={18}
+                        />
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                    </section>
+                </li>
+                <li>
+                    <Image
+                        src={"/axie-provisory.png"}
+                        alt={"Axie Name"}
+                        width={50}
+                        height={40}
+                    />
+                    <section>
+                        <Image
+                            src={"/rune-provisory.png"}
+                            alt={"Axie Parts name"}
+                            width={18}
+                            height={18}
+                        />
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                    </section>
+                </li>
+                <li>
+                    <Image
+                        src={"/axie-provisory.png"}
+                        alt={"Axie Name"}
+                        width={50}
+                        height={40}
+                    />
+                    <section>
+                        <Image
+                            src={"/rune-provisory.png"}
+                            alt={"Axie Parts name"}
+                            width={18}
+                            height={18}
+                        />
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                        <Image src={"/rune-provisory.png"} alt={"Axie Parts name"} width={18} height={18}/>
+                    </section>
+                </li>
+            </ul>
+
         </li>
     )
 }
--- src/components/cardsHolder/top3OriginsPlayers/index.tsx
+++ src/components/cardsHolder/top3OriginsPlayers/index.tsx
@@ -1,11 +1,21 @@
 import Top3OriginsPlayerCard from "@/components/cards/top3OriginsPlayer"
+import styles from "./styles.module.scss"
+import Link from "next/link"
 
 const Top3OriginsPlayersCardsHolder = () => {
 
     return (
-        <ul>
-            <Top3OriginsPlayerCard/>
-        </ul>
+        <section className={styles.container}>
+            <h2>Top 3 Origins Players</h2>
+            <ul className={styles.cardsHolder}>
+                <Top3OriginsPlayerCard/>
+            </ul>
+            <Link
+                href={"#"}
+            >
+                VIEW FULL LEADERBOARD
+            </Link>
+        </section>
     )
 
 }
